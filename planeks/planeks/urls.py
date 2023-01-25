9e5995d8fd72df0755fceaa538edf1e83a57040e
artem.loginov <artem.loginov@audienzz.ch>
2023-01-25 15:31:46 +0200

    Add login window.

--- planeks/planeks/urls.py
+++ planeks/planeks/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('accounts/login/', auth_views.LoginView.as_view()),
     path("admin/", admin.site.urls),
 ]
